32b7ea7558ddca86774e20aac011228636a3accc
Felipe Diaz <luisfe.diazc@gmail.com>
2021-05-14 20:35:20 -0500

    hadoop mapreduce homework

---
+++ hadoop-mapreduce/README.md
@@ -0,0 +1,22 @@
+**Escuela Colombiana de Ingeniería Julio Garavito**,
+**Algorithms and data representation**,
+
+**Homework # 1. Professor Cristian C. Garzón Alfonso**
+
+Luis Felipe Díaz Chica - Master's degree student
+
+   
+#### How to run this notebook
+* install python3 and pip
+* then run:
+`pip install -r requirements.txt`
+* run `jupyter notebook`
+* go to `localhost:8888`
+* run this notebook cells
+
+#### Proecedure to generate the data files
+
+* Each of the following charts is using a data file generated from a mapreduce script that was executed in a local Hadoop cluster. 
+* The input for all the mapreduce scripts was a file with tweets and retweets open data.
+* For more information about the scripts fo to the `tweetswordcount` project in the root of this project.
+
---
+++ hadoop-mapreduce/tweetwordscount/src/main/java/org/example/AllowedWordsTweets.java
@@ -0,0 +1,100 @@
+package org.example;
+
+import org.apache.hadoop.io.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class AllowedWordsTweets {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+ 
+        private Set<String> allowedWords = new HashSet<>(Arrays.asList(
+                "TRUMP",
+                "DICTATOR",
+                "MAGA",
+                "IMPEACH",
+                "DRAIN",
+                "SWAP",
+                "CHANGE"
+        ));
+        
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            Reader inputString = new StringReader(value.toString());
+            BufferedReader br = new BufferedReader(inputString);
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+                
+                JSONObject obj = (JSONObject) JSONValue.parse(line);
+                String message = (String) obj.get("text");
+                String[] words = message.split(" ");
+                for(String wd: words) {
+                    if(allowedWords.contains(wd.toUpperCase())){
+                        word.set(wd.toUpperCase());
+                        context.write(word, one);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(AllowedWordsTweets.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
